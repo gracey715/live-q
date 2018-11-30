d16a79e8532b5d975911fd6bd12372a9e2e55a35
Grace Yang <graceyang@10-17-215-131.dynapool.wireless.nyu.edu>
2018-11-30 17:21:14 -0500

    README update

--- postgres/liveq-create.sql
+++ postgres/liveq-create.sql
@@ -1,30 +1,30 @@
 DROP TABLE IF EXISTS restaurant;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS event;
 
--- Contains userIDs of users that checked in, their party size
-CREATE TABLE restaurant {
-	restaurant_id SERIAL,
-	user_id INTEGER REFERENCES user(user_id),
-	party_size INTEGER NOT NULL,
-	PRIMARY KEY(restaurant_id)
-}
-
-CREATE TABLE user {
+CREATE TABLE customer(
 	-- User ID will be the phone number they use to check in
-	user_id INTEGER NOT NULL,
+	customer_id INTEGER NOT NULL,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
-	PRIMARY KEY(user_id)
-}
+	PRIMARY KEY(customer_id)
+);
+
+-- Contains userIDs of users that checked in, their party size
+CREATE TABLE restaurant(
+	restaurant_id SERIAL,
+	customer_id INTEGER REFERENCES customer(customer_id),
+	party_size INTEGER NOT NULL,
+	PRIMARY KEY(restaurant_id)
+);
 
 -- Contains information on each "event", ie. one instance of a party checking in to the restaurant
-CREATE TABLE event {
+CREATE TABLE event(
 	event_id SERIAL,
 	restaurant_id INTEGER REFERENCES restaurant(restaurant_id),
-	time_joined TIMESTAMP NOT NULL,
-	time_served TIMESTAMP NOT NULL,
+	time_joined TIMESTAMPTZ NOT NULL,
+	time_served TIMESTAMPTZ NOT NULL,
 	party_size INTEGER NOT NULL,
 	position INTEGER NOT NULL,
 	PRIMARY KEY (event_id)
-}
+);
