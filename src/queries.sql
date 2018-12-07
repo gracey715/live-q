44b834df0a053a11e7980f8fca13918de65e376f
Grace Yang <graceyang@10-17-141-72.dynapool.wireless.nyu.edu>
2018-12-07 18:38:31 -0500

    SQL queries

---
+++ src/queries.sql
@@ -0,0 +1,15 @@
+-- QUERIES FOR EVENT TABLE
+-- Insert a new event into the event table
+INSERT INTO event(restaurant_id, time_joined, time_served, party_size, position) VALUES (?, ?, ?, ?, ?);
+
+-- Update time_served when a party is served
+UPDATE event SET time_served = ? WHERE event_id = ?;
+
+
+
+-- QUERIES FOR WAIT_TIMES TABLE
+-- Get the average wait time for a certain position in line
+SELECT avg(estimated_wait) FROM wait_times WHERE position = ?;
+
+-- Update the estimated wait time for a position
+UPDATE wait_times SET estimated_wait = ? WHERE position = ?;
